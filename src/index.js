e68fb331a76ae5f312ea4da069d5e05c0e5d7d74
GMV <gmv@GMVs-MacBook-Pro-4.local>
2020-02-12 17:47:09 +0700

    menambahkan fitur error message

--- src/index.js
+++ src/index.js
@@ -443,23 +443,68 @@ import Card from './components/components/Card';
 //     }
 // }
 
+// class MyForm extends React.Component {
+//     constructor(props) {
+//         super(props) ;
+//             this.state = {
+//                 username: '',
+//                 age: null,
+//             };
+//         }
+
+//         myChangeHandler = (event) => {
+//             let nam = event.target.name;
+//             let val = event.target.value;
+//             if(nam === "age") {
+//                 if (!Number(val)) {
+//                     alert("Your age must be a number");
+//                 }
+//             }
+//             this.setState({[nam] : val})
+//         }
+
+//         render() {
+//             return (
+//                 <form>
+//                     <h1>Hello {this.state.username} {this.state.age}</h1>
+//                     <p>Enter your name:</p>
+//                     <input
+//                         type = 'text'
+//                         name = 'username'
+//                         onChange={this.myChangeHandler}
+//                     />
+//                     <p>Enter your age:</p>
+//                     <input
+//                         type='text'
+//                         name = 'age'
+//                         onChange={this.myChangeHandler}
+//                     />
+//                 </form>
+//             );
+//         }
+
+//     }
+
 class MyForm extends React.Component {
     constructor(props) {
         super(props) ;
             this.state = {
                 username: '',
                 age: null,
+                errormessage: ''
             };
         }
 
         myChangeHandler = (event) => {
             let nam = event.target.name;
             let val = event.target.value;
-            if(nam === "age") {
-                if (!Number(val)) {
-                    alert("Your age must be a number");
+            let err = '';
+            if (nam === "age") {
+                if(val !="" && !Number(val)){
+                    err = <strong>Your age must be a number</strong>
                 }
             }
+            this.setState({errormessage: err})
             this.setState({[nam] : val})
         }
 
@@ -479,6 +524,8 @@ class MyForm extends React.Component {
                         name = 'age'
                         onChange={this.myChangeHandler}
                     />
+
+                    {this.state.errormessage}
                 </form>
             );
         }
